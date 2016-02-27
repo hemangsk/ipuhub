b4faf236bf48b130e047a4a4369f10e9fd664af2
Hemang Kumar <hemangsk@gmail.com>
2016-02-27 13:22:13 +0530

    version 2 chemchord and ipubar

--- qa/inc/languages/english/imgur.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Imgur.com plugin : allow to upload an image to imgur
- * and add it in the post
- */
- 
-$l['imgur_open'] = "New Imgur Pic";
-$l['imgur_ptitle'] = "Upload to Imgur";
-$l['imgur_select'] = "Select file...";
